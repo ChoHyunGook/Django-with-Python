857a4fc5e93cbe09167529074b8505b0b3aaf6c7
ChoHyunGook <whgusrnr7755@gmail.com>
2022-03-18 13:17:22 +0900

    2022-03-18 13:16 **kwargs/namecutting

--- titanic/models.py
+++ titanic/models.py
@@ -16,32 +16,32 @@ class TitanicModels(object):
         this.label=this.train['Survived']
         this.train = this.train.drop('Survived', axis=1)
         this=self.drop_feature(this,'Cabin', 'Parch', 'Ticket', 'SibSp')
+        #self.kwargs_sample(name='이순신')
+        this = self.name_nominal(this)
+
         '''
         this=self.create_train(this)
-        this=self.name_nominal(this)
         this=self.sex_nominal(this)
         this=self.age_ratio(this)
         this=self.embarked_nominal(this)
         this=self.pclass_ordinal(this)
         this=self.fare_ratio(this)
         '''
-        self.print_this(this)
+        self.df_info(this)
         return this
 
     @staticmethod
-    def print_this(this):
-        ic(f'1. Train의 타입 {type(this.train)}\n')
-        ic(f'2. Train의 컬럼 {this.train.columns}\n')
-        ic(f'3. Train의 상위1개 {this.train.head(1)}\n')
-        ic(f'4. Train의 null의 개수 {this.train.isnull().sum()}\n')
-        ic(f'5. Test의 타입 {type(this.test)}\n')
-        ic(f'6. Test의 컬럼 {this.test.columns}\n')
-        ic(f'7. Test의 상위1개 {this.test.head(1)}\n')
-        ic(f'8. Test의 null의 개수 {this.test.isnull().sum()}\n')
+    def id_info(this):
         ic(f'9. id의 타입 {type(this.id)}\n')
-        ic(f'9. id의 상위 10개 {type(this.id[:10])}\n')
-        print('*'*100)
+        ic(f'9. id의 상위 3개 {type(this.id[:3])}\n')
 
+    @staticmethod
+    def df_info(this):
+        [ic(f'{i.info()}')for i in [this.train,this.test]]
+
+    @staticmethod
+    def null_check(this):
+        [ic(f'{i.isnull().sum()}')for i in [this.train,this.test]]
 
     def create_this(self,dataset)->object:
         this = dataset
@@ -59,15 +59,11 @@ class TitanicModels(object):
     @staticmethod
     def drop_feature(this,*feature)->object:
         #[i.drop(j, axis=1, inplace=True)for j in feature for i in [this.train,this.test]]
-        #[i.drop(list(feature), axis = 1)for i in [this.train, this.test]]
+        [i.drop(list(feature), axis = 1)for i in [this.train, this.test]]
         # 리스트로 형태를 바꿔주는게 a
-        a=[i for i in feature]
+        '''a=[i for i in feature]
         this.train=this.train.drop(a, axis=1)
         this.test=this.test.drop(a,axis=1)
-
-        '''
-        this.train = this.train.drop([i for i in feature], axis=1)
-        this.test = this.test.drop([i for i in feature], axis=1)
         '''
         return this
 
@@ -77,12 +73,15 @@ class TitanicModels(object):
         interval(상대) vs ratio(절대적인기준)'''
 
     @staticmethod
-    def pclass_ordinal(this)->object:
-
+    def name_nominal(this) -> object:
+        combine = [this.train,this.test]
+        for dataset in combine:
+            dataset['Title']=dataset.Name.str.extract('([A-Za-z]+)\.',expand=False)
+            ic(dataset['Title'])
         return this
 
     @staticmethod
-    def name_nominal(this)->object:
+    def pclass_ordinal(this)->object:
         return this
 
     @staticmethod
@@ -100,3 +99,10 @@ class TitanicModels(object):
     @staticmethod
     def embarked_nominal(this)->object:
         return this
+
+
+
+    @staticmethod
+    def kwargs_sample(**kwargs)->None:
+        ic(type(kwargs))
+        {print(''.join(f'key:{i},val:{j}'))for i,j in kwargs.items()}#key:name,val:이순신
